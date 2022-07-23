b0f1f81802dede5a35989b4559235de09a56a92d
Jaime <jaime99dr@gmail.com>
2022-07-23 17:10:45 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+// COMPONENTS
+import { MainViewComponent } from './components/main-view/main-view.component';
+
+const routes: Routes = [
+  { path: '', component: MainViewComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
