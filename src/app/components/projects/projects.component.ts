b95b30255894371b9fdd76f1d42d8da5cc3326df
Jaime <jaime99dr@gmail.com>
2022-09-19 12:36:42 +0100

    carousel done

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent {
+  slides: any[] = [
+    {
+      'img': '../../../assets/ip-address-tracker.PNG',
+      'alt': 'Ip Address Tracker'
+    },
+    {
+      'img': '../../../assets/remote-work.PNG',
+      'alt': 'Remote Work'
+    },
+    {
+      'img': '../../../assets/job-listing.PNG',
+      'alt': 'job-listing'
+    },
+    {
+      'img': '../../assets/TFT.png',
+      'alt': 'tft'
+    }
+  ];
+  slideIndex: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  showSlide(index: number) {
+    this.slideIndex = index;
   }
 
+  prev() {
+    this.slideIndex = (this.slideIndex - 1 + this.slides.length) % this.slides.length;
+    console.log("slideIndex: " + this.slideIndex);
+  }
+
+  next() {
+    this.slideIndex = (this.slideIndex + 1) % this.slides.length;
+    console.log("slideIndex: " + this.slideIndex);
+  }
 }
