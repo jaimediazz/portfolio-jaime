f2fe4ba15b895d3c7e3bdbcfcd144706c249c731
Jaime <jaime99dr@gmail.com>
2022-09-22 17:35:21 +0100

    cv language depending on locale selected done

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.css']
 })
-export class MainViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MainViewComponent {
+  cvPath: string = $localize `assets/CV_Jaime_Díaz_en.pdf`;
+  
 }
